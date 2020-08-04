01a4edac0206610d3975b88fdab39f3c96cd5cfa
Myst <1592048+Mystou@users.noreply.github.com>
2020-08-04 16:36:49 +0200

    Clean directories

--- website/src/app/ItemBaseComponent.ts
+++
@@ -1,9 +0,0 @@
-import { Input, Directive } from '@angular/core';
-
-import { SCItem } from './SCItem';
-
-@Directive()
-export class ItemBaseComponent {
-  @Input()
-  item: SCItem | undefined;
-}
--- website/src/app/ItemIndexEntry.ts
+++
@@ -1,9 +0,0 @@
-export interface ItemIndexEntry {
-	name: string;
-	type: string;
-	subType: string;
-	size: number | undefined;
-	grade: number | undefined;
-	manufacturer: string;
-	className: string;
-}
