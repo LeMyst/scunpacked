072c43bcb64f8944f519395a96a4c47b76488af4
Myst <1592048+Mystou@users.noreply.github.com>
2020-08-04 16:39:05 +0200

    Revert "Merge branch 'rework-locations'"
    This reverts commit ccf5561b6336c6efa2c45cc77b532bcf115d8502, reversing
    changes made to 01a4edac0206610d3975b88fdab39f3c96cd5cfa.

--- Loader/Program.cs
+++ Loader/Program.cs
@@ -129,13 +129,13 @@ namespace Loader
 			var starmapIndex = starmapLoader.Load();
 			File.WriteAllText(Path.Combine(outputRoot, "starmap.json"), JsonConvert.SerializeObject(starmapIndex));
 
-			/*// Location
+			// Location
 			var locationLoader = new LocationLoader(new LocalisationService(labels))
 			{
 				DataRoot = scDataRoot
 			};
 			var locationIndex = locationLoader.Load();
-			File.WriteAllText(Path.Combine(outputRoot, "locations.json"), JsonConvert.SerializeObject(locationIndex));*/
+			File.WriteAllText(Path.Combine(outputRoot, "locations.json"), JsonConvert.SerializeObject(locationIndex));
 		}
 	}
 }
